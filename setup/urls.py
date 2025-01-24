ddd03a5e1c85d26f3be1fb92bef19934918ce88a
Victor Martinez <victorjbamartinez@gmail.com>
2025-01-24 10:32:32 -0300

    END OF THE VIDEO - LAST CHANGES

--- setup/urls.py
+++ setup/urls.py
@@ -5,6 +5,7 @@ from todos.views import TodoListView
 from todos.views import TodoCreateView
 from todos.views import TodoUpdateView
 from todos.views import TodoDeleteView
+from todos.views import TodoCompleteView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,4 +13,5 @@ urlpatterns = [
     path("create", TodoCreateView.as_view(), name="todo_create"),
     path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
     path("delete/<int:pk>", TodoDeleteView.as_view(), name="todo_delete"),
+    path("complete/<int:pk>", TodoCompleteView.as_view(), name="todo_complete"),
 ]
--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.db import models
 
 
@@ -7,3 +8,11 @@ class Todo(models.Model):
     created_at  = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     deadline    = models.DateField(verbose_name="Data de Entrega", null=False, blank=False)
     finished_at = models.DateField(null=True, blank=False)
+
+    class Meta:
+        ordering = ["deadline"]
+
+    def mark_as_complete(self):
+        if not self.finished_at:
+            self.finished_at = date.today()
+            self.save()
