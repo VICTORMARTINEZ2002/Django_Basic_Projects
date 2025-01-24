629847c209baf8de6bdf07aeb148997cd9c47b24
Victor Martinez <victorjbamartinez@gmail.com>
2025-01-24 10:04:10 -0300

    Add Editar Tarefa

--- setup/urls.py
+++ setup/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from todos.views import TodoListView, TodoCreateView
+from todos.views import TodoListView
+from todos.views import TodoCreateView
+from todos.views import TodoUpdateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TodoListView.as_view(), name="todo_list"),
     path("create", TodoCreateView.as_view(), name="todo_create"),
+    path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update")
 ]
