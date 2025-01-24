d8545f2efac1d01c683b5501e1fcba2b4e443348
Victor Martinez <victorjbamartinez@gmail.com>
2025-01-24 10:15:59 -0300

    Add Excluir Tarefa

--- setup/urls.py
+++ setup/urls.py
@@ -4,10 +4,12 @@ from django.urls import path
 from todos.views import TodoListView
 from todos.views import TodoCreateView
 from todos.views import TodoUpdateView
+from todos.views import TodoDeleteView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TodoListView.as_view(), name="todo_list"),
     path("create", TodoCreateView.as_view(), name="todo_create"),
-    path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update")
+    path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
+    path("delete/<int:pk>", TodoDeleteView.as_view(), name="todo_delete"),
 ]
